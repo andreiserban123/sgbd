2df457be4cddd985c9f67f6f63c6ef1374748a17
SERBAN EC ANDREI <andyspeed2003@gmail.com>
2024-03-27 13:06:52 +0200

    tema 2 finished

--- sgbd.sql
+++ sgbd.sql
@@ -128,4 +128,91 @@ exception
     when INSERT_EXCP then
         dbms_output.put_line('Nu s-a putut insera userul');
 end;
---7. 
+--7. afiseaza toti angajatii cu id-ul intre 100 si 200
+declare
+    v_rec ANGAJATI%rowtype;
+    my_ex exception ;
+begin
+    delete from ANGAJATI where ID_ANGAJAT = 105;
+    if sql%notfound then
+        raise my_ex;
+    end if;
+    for i in 100..200
+        loop
+            select * into v_rec from ANGAJATI where ID_ANGAJAT = i;
+            DBMS_OUTPUT.PUT_LINE(v_rec.ID_ANGAJAT || ' ' || v_rec.EMAIL);
+        end loop;
+exception
+    when my_ex then
+        DBMS_OUTPUT.PUT_LINE('Nu exista angajatul cu id-ul 105');
+    when no_data_found then
+        DBMS_OUTPUT.PUT_LINE('Angajatul cu id-ul: ' || (v_rec.ID_ANGAJAT + 1) || ' nu s-a gasit');
+end;
+
+--8. afiseaza numele studentului ,nota si materia daca este o nota para afiseaza si prenumele
+declare
+    cursor c is
+        select F_NAME, L_NAME, NOTA, NAME nume_mat
+        from SIT_PERSON
+                 join SIT_USER on SIT_PERSON.ID = SIT_USER.ID_PERS
+                 join
+             SIT_USER_ROLES on SIT_USER.ID = SIT_USER_ROLES.ID_USER
+                 join SIT_CATALOGUE on SIT_USER_ROLES.ID = SIT_CATALOGUE.ID_STUDENT
+                 join SIT_SUBJECT on SIT_CATALOGUE.ID_MATERIE = SIT_SUBJECT.ID
+                 join SIT_MATERII on SIT_SUBJECT.ID_MATERIE = SIT_MATERII.ID
+        where SIT_USER_ROLES.ID_ROLE = 1;
+begin
+    for rec in c
+        loop
+            if rec.NOTA mod 2 = 0 then
+                DBMS_OUTPUT.PUT_LINE(rec.F_NAME || ' ' || rec.L_NAME || ' a luat ' || rec.nota || ' la ' ||
+                                     rec.nume_mat);
+            else
+                DBMS_OUTPUT.PUT_LINE(rec.L_NAME || ' a luat ' || rec.nota || ' la ' ||
+                                     rec.nume_mat);
+            end if;
+        end loop;
+exception
+    when others then raise_application_error(-00001, sqlerrm);
+end;
+/
+
+
+--9. afiseaza toti studentii de clasa a -9a care au media peste 5 la matematica
+declare
+    cursor c is
+        select F_NAME, L_NAME, grade clasa, NAME nume_mat, round(avg(NOTA)) medie
+        from SIT_PERSON
+                 join SIT_USER on SIT_PERSON.ID = SIT_USER.ID_PERS
+                 join
+             SIT_USER_ROLES on SIT_USER.ID = SIT_USER_ROLES.ID_USER
+                 join SIT_CATALOGUE on SIT_USER_ROLES.ID = SIT_CATALOGUE.ID_STUDENT
+                 join SIT_SUBJECT on SIT_CATALOGUE.ID_MATERIE = SIT_SUBJECT.ID
+                 join SIT_MATERII on SIT_SUBJECT.ID_MATERIE = SIT_MATERII.ID
+                 join SIT_CLASS on SIT_SUBJECT.ID_CLASS = SIT_CLASS.ID
+                 join SIT_GRADE on SIT_CLASS.GRADE_ID = SIT_GRADE.ID
+        where SIT_USER_ROLES.ID_ROLE = 1
+          and GRADE = 9
+          and SIT_MATERII.NAME = 'MATEMATICA'
+        group by F_NAME, L_NAME, grade, NAME;
+    v_rec c%rowtype;
+begin
+    open c;
+    loop
+        fetch c into v_rec;
+        exit when c%notfound;
+        if v_rec.medie >= 5 then
+            DBMS_OUTPUT.PUT_LINE(v_rec.F_NAME || ' ' || v_rec.L_NAME || ' din clasa a-' || v_rec.clasa ||
+                                 'a are media la ' ||
+                                 lower(v_rec.nume_mat) || ' ' || v_rec.medie);
+        end if;
+    end loop;
+    close c;
+exception
+    when others then
+        if c%isopen then
+            close c;
+        end if;
+        DBMS_OUTPUT.PUT_LINE(sqlerrm);
+end;
+/
